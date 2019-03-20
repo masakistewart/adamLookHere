5903373a07504e98cf5d5239cdbd731f1894daad
Cairo Stewart <stewartcairo@gmail.com>
2019-03-20 15:44:14 -0700

    add graphql and express-graphql npm pkgs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql')
+
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+
 
 app.listen(PORT, () => {
     console.log(`I am listening on PORT: ${PORT}`);
