e3efe44dc9c4ba878dee6a0a2c1bf5adaacf49d3
Cairo Stewart <stewartcairo@gmail.com>
2019-03-22 12:30:03 -0700

    ask me about my mistake

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@ const graphqlHTTP = require('express-graphql');
 const request = require('request');
 const schema = require('./schema/schema');
 
-const credentials = Buffer.from(`${process.env.CLIENTID}:${process.env.CLIENTSECRET}`).toString('base64');
-const authStr = `Basic ${credentials}`;
-
 const PORT = process.env.PORT || 8080;
 const app = express();
 
--- server/requests/Spotify.js
+++ server/requests/Spotify.js
@@ -1,41 +1,55 @@
 const request = require('request');
-const searchConfig = { method: 'GET', url: `https://api.spotify.com/v1/search?q=${str}&type=${type.join(',')}` }
-const config = {
-    method: 'POST',
-    url: 'https://accounts.spotify.com/api/token',
-    headers:
-    {
-        Authorization: 'Basic ZjhmNDllZmFkMGExNGFlNjhmODFlMWIyOGVkOGZmYzA6MmE2MzBiNGM5ZjMzNDA5NTk2MmI0MDNjNjdkNjI3YmQ=',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    form: { grant_type: 'client_credentials' }
-};
+
+const credentials = Buffer.from(`${process.env.CLIENTID}:${process.env.CLIENTSECRET}`).toString('base64');
+const authStr = `Basic ${credentials}`;
 
 class Spotify {
     constructor() {
+        this.fetchTokenConfig = {
+            method: 'POST',
+            url: 'https://accounts.spotify.com/api/token',
+            headers:
+            {
+                Authorization: authStr,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            form: { grant_type: 'client_credentials' }
+        }
+
         this.token = this.fetchToken();
     }
 
-    fetchToken() {
-        return new Promise((resolve, reject) => {
-            if (process.env.CLIENTID || process.env.CLIENTSECRET) {
-                reject(new Error('MISSING CLIENT ID AND CLIENT SECRET'))
-            }
+fetchToken() {
+    return new Promise((resolve, reject) => {
+        if (!(process.env.CLIENTID || process.env.CLIENTSECRET)) {
+            reject(new Error('MISSING CLIENT ID AND CLIENT SECRET'))
+        }
 
-            request(config, (error, _, body) => {
-                if (error) reject(new Error(error));
-                resolve(body)
-            });
+        request(this.fetchTokenConfig, (err, _, body) => {
+            if (err) reject(new Error(error));
+            console.log(body)
+            resolve(body)
+        });
 
-        })
-    }
+    })
+}
 
-    find(str, type) {
-        return new Promise((resolve, reject) => {
-            request(searchConfig, (err, _, body) => { if (error) reject(new Error(error)); resolve(body)})
-        })
-    }
+async find(str, arr) {
+    const searchConfig = {
+        method: 'GET',
+        url: `https://api.spotify.com/v1/search?q=${str}&type=${arr.join(',')}`,
+        headers: {
+            Authorization: 'Bearer ' + await this.token
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        request(searchConfig, (err, _, body) => { if (err) reject(new Error(err)); resolve(body) })
+    })
+}
 }
 
 const instance = new Spotify();
-console.log(instance.token)
+instance.find('hello', ['alternative']).then(data => {
+    console.log(data)
+})
